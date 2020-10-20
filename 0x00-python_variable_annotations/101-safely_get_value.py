0763e701d7527ec3d321b1d67e33a65df68f5baa
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-10-19 22:51:24 -0500

    task 11 More involved type annotations

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -4,9 +4,9 @@ from typing import Any, Union, Mapping, TypeVar
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, 
-                        default: Union[T, None] = None) -> Union[Any, T]:
-    
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
+    """ safely get value """
     if key in dct:
         return dct[key]
     else:
