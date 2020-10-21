ec8549e869e1d4ba68ce9e3e648b0a438af3f99c
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-10-21 15:29:35 -0500

    dont know why it fails

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """ Let's execute multiple coroutines at the same time """
-import asyncio
 import time
+import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
     """ measures the total execution time """
-    a: float = time.time()
+    start: float = time.time()
     asyncio.run(wait_n(n, max_delay))
-    b: float = time.time()
-    return (b - a) / n
+    end: float = time.time()
+    return (end - start) / n
