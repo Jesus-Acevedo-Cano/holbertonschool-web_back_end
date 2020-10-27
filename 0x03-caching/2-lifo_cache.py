74f91b676f833b111d77821161a2fabfff493288
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-10-26 23:53:19 -0500

    task 2 lifo cache

--- 0x03-caching/2-lifo_cache.py
+++ 0x03-caching/2-lifo_cache.py
@@ -9,17 +9,19 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """ constructor """
         self.order = []
+        self.discard = None
         super().__init__()
 
     def put(self, key, item):
         """ Add an item in the cache """
         if key and item:
             self.cache_data[key] = item
+            self.discard = key
             self.order.append(key)
 
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            del self.cache_data[self.order[len(self.cache_data) - 1]]
-            print("DISCARD: {}".format(self.order[len(self.cache_data) - 1]))
+            del self.cache_data[self.discard]
+            print("DISCARD: {}".format(self.discard))
 
     def get(self, key):
         """ Get an item by key """
