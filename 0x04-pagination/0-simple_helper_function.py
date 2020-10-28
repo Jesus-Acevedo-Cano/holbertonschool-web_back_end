b215e90cc02ab40928b3c8059f3c9401292d6d5e
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-10-28 00:07:44 -0500

    init files

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -4,6 +4,6 @@
 
 def index_range(page: int, page_size: int) -> tuple:
     """ return start index and end index """
-    start: int = (page -1) * page_size
+    start: int = (page - 1) * page_size
     end: int = page_size * page
     return (start, end)
