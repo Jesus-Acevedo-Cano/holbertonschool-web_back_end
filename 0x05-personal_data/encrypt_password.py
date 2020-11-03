f7a8b79ebcbcef1cc418cd2ca7bb1b3331ec8631
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-11-02 23:40:55 -0500

    task 5 hashed pass

---
+++ 0x05-personal_data/encrypt_password.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Encrypting passwords """
+import bcrypt
+
+def hash_password(password: str) -> bytes:
+    """ transform to hashed password """
+    password = bytes(password, 'utf-8')
+    return bcrypt.hashpw(password, bcrypt.gensalt())
