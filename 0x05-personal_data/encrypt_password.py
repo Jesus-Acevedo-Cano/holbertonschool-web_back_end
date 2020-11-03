ae2eb9c46d98c31872ce289f19c9ae4b3d86c125
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-11-02 23:44:24 -0500

    task 6 check pass

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -7,3 +7,8 @@ def hash_password(password: str) -> bytes:
     """ transform to hashed password """
     password = bytes(password, 'utf-8')
     return bcrypt.hashpw(password, bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ validate that the provided password matches the hashed password """
+    return bcrypt.checkpw(bytes(password, 'utf-8'), hashed_password)
