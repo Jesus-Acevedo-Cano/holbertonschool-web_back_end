25211529268e7505d0ff0f8af621bc2d5fa4e155
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-11-06 23:27:53 -0500

    modified file task 10

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """ class to manage the API basic authentication """
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -47,3 +49,17 @@ class BasicAuth(Auth):
         user = decoded_base64_authorization_header[:colons_index]
         password = decoded_base64_authorization_header[colons_index + 1:]
         return (user, password)
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str
+                                     ) -> TypeVar('User'):
+        """ returns the User instance based on his email and password """
+        if user_email is None or type(user_email) is not str:
+            return None
+        if user_pwd is None or type(user_pwd) is not str:
+            return None
+        user = User.search({'email': user_email})
+        if not user or user == '':
+            return None
+        if user[0].is_valid_password(user_pwd) is False:
+            return None
+        return user[0]
