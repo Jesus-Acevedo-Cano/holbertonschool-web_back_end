a6e909239e5d8a40ffae53d71e6355eaa403b11c
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-11-11 01:25:00 -0500

    modified file task 4

--- 0x07-Session_authentication/api/v1/auth/auth.py
+++ 0x07-Session_authentication/api/v1/auth/auth.py
@@ -26,3 +26,10 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """ public method """
         return None
+
+    def session_cookie(self, request=None):
+        """ returns a cookie value from a request """
+        if request is None:
+            return None
+        session_name = os.getenv('SESSION_NAME')
+        return request.cookies.get(session_name)
