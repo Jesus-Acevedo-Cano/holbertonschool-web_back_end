43dbe4b117d9229729217bd109312e332c178ee3
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-11-11 01:55:51 -0500

    modified file task 5

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -57,12 +57,12 @@ def before_request():
     if not auth.require_auth(request.path, paths_list):
         return
 
+    if auth.authorization_header(request) is None\
+       and auth.session_cookie(request) is None:
+        abort(401)
+
     if auth.current_user(request) is None:
         abort(403)
-
-    if not auth.authorization_header(request)\
-       and not auth.session_cookie(request):
-        abort(401)
 
     request.current_user = auth.current_user(request)
 
