ac61fde77da84488446a3513d026543c1d50298c
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-11-17 23:57:32 -0500

    modified file task 2

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -3,6 +3,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from user import Base
 from user import User
 
@@ -28,4 +30,12 @@ class DB:
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """  takes in arbitrary keyword arguments
+        and returns the first row found in the users """
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound
         return user
