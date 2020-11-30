b0ff45c16d15488f44637c05f8ebdc7ac6c9c611
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-11-29 23:57:15 -0500

    task 4

--- 0x0A-i18n/4-app.py
+++ 0x0A-i18n/4-app.py
@@ -20,12 +20,15 @@ app.config.from_object(Config)
 @app.route('/')
 def hello():
     """ Hello world """
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 @babel.localeselector
 def get_locale():
     """ determine the best match with supported languages """
+    locale = request.args.get("locale")
+    if locale in app.config['LANGUAGES']:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
