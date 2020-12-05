dc06bfccbdbc7b682f6ddbdb361a497f157e29c7
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-12-04 23:07:37 -0500

    divide func in mysql

---
+++ 0x0C-MySQL_Advanced/10-div.sql
@@ -0,0 +1,8 @@
+-- Safe divide
+-- divides (and returns) the first by the second number
+DELIMITER $$
+CREATE FUNCTION SafeDiv ( a INT, b INT)
+RETURN FLOAT
+BEGIN IF b == 0 THEN RETURN 0; END IF;
+RETURN a / b; 
+$$
