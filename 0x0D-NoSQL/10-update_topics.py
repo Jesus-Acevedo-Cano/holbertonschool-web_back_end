3b428939850267981af74dfeb0c6f8525e82f6c5
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-12-08 23:43:27 -0500

    update tasks

---
+++ 0x0D-NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Change school topics """
+
+
+def update_topics(mongo_collection, name, topics):
+    """ changes all topics of a school document based on the name """
+    myquery = { "name": { name } }
+    newvalues = { "$set": { "topics":topics } }
+    return mongo_collection.update_many(myquery, newvalues)
