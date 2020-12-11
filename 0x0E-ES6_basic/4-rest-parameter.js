6475474fceb68352e76b7bba6ae5c7c02984ac63
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-12-11 02:31:16 -0500

    rest parameter correction

--- 0x0E-ES6_basic/4-rest-parameter.js
+++ 0x0E-ES6_basic/4-rest-parameter.js
@@ -1,7 +1,3 @@
 export default function returnHowManyArguments(...args) {
-  let argsCount = 0;
-  /* eslint-disable no-unused-vars */
-  for (const arg of args) argsCount += 1;
-  /* eslint-disable no-unused-vars */
-  return argsCount;
+  return args.length;
 }
