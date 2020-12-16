297f793c68a48b5ba32ad62f760558b81b657d77
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-12-16 01:17:42 -0500

    multiple successful promises

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -5,5 +5,5 @@ import createUser from "./utils"; */
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()]).then((values) => {
     console.log(values[0].body, values[1].firstName, values[1].lastName);
-  }).catch(() => Error('Signup system offline'));
+  }).catch(() => console.log('Signup system offline'));
 }
